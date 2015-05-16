dc73671e7b79cefad8fba94628e05b047cbf691e
Matthew Russell <matthew.g.russell@gmail.com>
2015-05-16 10:47:24 -0400

    Started implemented ORM classes, following the interfaces I made earlier.  Slowly getting there..  So far Empire is using the ORM, Territory is about to be.

--- Empire.php
+++ Empire.php
@@ -3,6 +3,7 @@
 namespace DiplomacyOrm;
 
 use DiplomacyOrm\Base\Empire as BaseEmpire;
+use DiplomacyEngine\Empires\iEmpire;
 
 /**
  * Skeleton subclass for representing a row from the 'empire' table.
@@ -14,7 +15,38 @@ use DiplomacyOrm\Base\Empire as BaseEmpire;
  * long as it does not already exist in the output directory.
  *
  */
-class Empire extends BaseEmpire
-{
+class Empire extends BaseEmpire implements iEmpire {
+
+	public function __construct($id, $name_official, $name_long, $name_short) {
+		parent::__construct();
+		$this->setEmpireId($id);
+		$this->setNameOfficial($name_official);
+		$this->setNameLong($name_long);
+		$this->setNameShort($name_short);
+	}
+
+	public function __toString() {
+		return $this->getNameShort();
+	}
+
+	public function getId() {
+		return $this->getId();
+	}
+
+	/**
+	 * Reads in a JSON object with all the empires defined.
+	 * [{id: 'CAN', name_official: 'Canada', name_long: 'Dominion of Canada', name_short: 'Canada' }, ...]
+	 */
+	public static function loadEmpires(array $objs) {
+		//print_r($objs);
+		$empires = array();
+		foreach ($objs as $obj) {
+			$t = new Empire($obj->id, $obj->name_official, $obj->name_long, $obj->name_short);
+			$empires[$t->getEmpireId()] = $t;
+		}
+		return $empires;
+	}
 
 }
+
+// vim: ts=3 sw=3 noet :
--- Territory.php
+++ Territory.php
@@ -3,6 +3,10 @@
 namespace DiplomacyOrm;
 
 use DiplomacyOrm\Base\Territory as BaseTerritory;
+use DiplomacyEngine\Territory\iTerritory;
+use DiplomacyEngine\Empires\Unit;
+
+//use DiplomacyEngine\Territory\Neighbours;
 
 /**
  * Skeleton subclass for representing a row from the 'territory' table.
@@ -14,7 +18,167 @@ use DiplomacyOrm\Base\Territory as BaseTerritory;
  * long as it does not already exist in the output directory.
  *
  */
-class Territory extends BaseTerritory
-{
+class Territory extends BaseTerritory implements iTerritory {
+	protected $state;
+	protected $occupier;
+	protected $unit;
+	protected $neighbours;
+
+	/**
+	 * Creates a new Territory
+	 *
+	 * @return iTerritory
+	 */
+	public static function create($id, $name, $type = TERR_LAND, $isSupply = false) {
+		$t = new Territory;
+		$t->setTerritoryId($id);
+		$t->setName($name);
+		$t->setType($type);
+
+		$t->setIsSupply($isSupply);
+
+		//$t->neighbours = new Neighbours();
+		$t->state = null;
+		$t->occupier = null;
+		$t->unit = null;
+
+		return $t;
+	}
+
+	public function __toString() {
+		return $this->name;
+	}
+
+	/**
+	 * Load this territory with the current game state.  From this
+	 * it can determine it's occupier, the game, turn, match, etc.
+	 */
+	public function setState(Match $match, Turn $turn) {
+		$this->state = StateQuery::findPk($match->getGame(), $match, $this);
+	}
+
+
+	/**
+	 * Who is occupying this territory
+	 *
+	 * @return iEmpire
+	 */
+	public function getOccupier() {
+		if (is_null($this->state)) trigger_error("Territory state is not set.  Use ::setState()");
+
+		return $this->state->getOccupier();
+	}
+
+	/**
+	 * Type of occupying unit
+	 *
+	 * @return Unit
+	 */
+	public function getUnit() {
+		if (is_null($this->state)) trigger_error("Territory state is not set.  Use ::setState()");
+
+		return $this->state->getUnit();
+	}
+
+	public function setOccupier(Empire $occupier, Unit $unit) {
+		if (is_null($this->state)) trigger_error("Territory state is not set.  Use ::setState()");
+
+		$this->state->setOccupier($occupier);
+		$this->state->setUnit($unit);
+	}
+
+	/** @return bool Check if $this is land */
+	public function isLand() { return $this->type == TerritoryTableMap::COL_TYPE_LAND; }
+
+	/** @return bool Check if $this is water */
+	public function isWater() { return $this->type == TerritoryTableMap::COL_TYPE_WATER; }
+
+	/**
+	 * Old system used constants, while propel uses enums, so overloading
+	 * this for backwards compatibility
+	 *
+	 * Maybe I should use COL_TYPE_LAND and COL_TYPE_WATER instead of magic
+	 * strings
+	 **/
+	public function setType($type) {
+		if ($type == TERR_LAND) {
+			parent::setType('land');
+		} elseif ($type == TERR_WATER) {
+			parent::setType('water');
+		} elseif (strtolower($type) === 'land' || strtolower($type) === 'water') {
+			parent::setType($type);
+		} else {
+			trigger_error("Attempted to set invalid territory type: $type");
+		}
+	}
+
+	/*
+	public function addNeighbour(iTerritory $neighbour) {
+		if (!array_key_exists($neighbour->getId(), $this->neighbours)) {
+			$this->neighbours[$neighbour->getId()] = $neighbour;
+
+			// The check above should prevent this from being an infinite loop
+			$neighbour->addNeighbour($this);
+		}
+	}
+	public function addNeighbours(array $neighbours) {
+		trigger_error('Not implemented');
+	}
+	public function getNeighbours() { return $this->neighbours; }
+	*/
+	public function isNeighbour(iTerritory $neighbour) {
+		return array_key_exists($neighbour->getId(), $this->neighbours);
+	}
+
+	/** @return bool Has Supply center */
+	/*
+	public function hasSupplyCenter() { return $this->is_supply; }
+	public function setSupplyCenter($hasSupply) {
+		$this->is_supply = $hasSupply;
+	}
+	*/
+
+	/**
+	 * Responsible for the initial loading of a territory into the game (not
+	 * the match)
+	 **/
+	public static function loadTerritories(array $empires, array $objs) {
+		$ts = array();
+		foreach ($objs as $obj) {
+			$t = self::create($obj->id, $obj->name, $obj->type, false); // TODO fix supply center
+
+			if (array_key_exists($obj->empire_start, $empires)) {
+				$t->setOccupier($empires[$obj->empire_start], new Unit($obj->starting_forces));
+			};
+
+			$ts[$t->getId()] = $t;
+		}
+
+		// Second pass, set up neighbours
+		foreach ($objs as $obj) {
+			$t = $ts[$obj->id];
+			foreach ($obj->neighbours as $nid) {
+				$n = $ts[$nid];
+				$t->addNeighbour($n);
+			}
+		}
+		return $ts;
+	}
+
+	/**
+	 * Quick territory lookup.  Written for backwards compatibility
+	 * with pre-ORM Diplomacy engine
+	 *
+	 * @return iTerritory
+	 */
+	public static function findTerritoryByName(Game $game, $name) {
+		return TerritoryQuery::create()
+			->filterByGame($game)
+			->findTerritoryByName($name)
+		;
+	}
+
 
 }
+
+// vim: ts=3 sw=3 noet :
